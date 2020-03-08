0ccb94901e1c4fbd75df36e93e14ce6298d91993
planeskent <31124290+planeskent@users.noreply.github.com>
2020-03-08 21:58:27 +0800

    add US part

--- driving.py
+++ driving.py
@@ -6,3 +6,9 @@ if country == "taiwan":
 		print("you can go to take driving exam")
 	else:
 		print("you can't take the driving exam yet")
+elif country == "USA":
+	if age >= 16:
+		print("you can go to take dirivng exam")
+	else:
+		print("you can't take the driving exam yet")
+		
